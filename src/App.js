e3e1ff6840cdddd277af668cbbe9795fcd0c97c2
savruk.maryan@gmail.com <savruk.maryan@gmail.com>
2021-07-28 16:33:35 +0300

    master-clear

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import React from "react";
-import Users from "./components/users/Users";
-
 
 function App() {
     return (
         <div>
-            <Users/>
+            ййййййййй
 
         </div>
     );
