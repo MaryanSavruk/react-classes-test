4c65c5b86c468302bd4ca1bf9abbf637c4dbae41
savruk.maryan@gmail.com <savruk.maryan@gmail.com>
2021-07-28 21:07:43 +0300

    less3-part2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import './App.css';
 import React from "react";
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
+import Users from "./components/users/Users";
+import Posts from "./components/posts/Posts";
+import Comments from "./components/comments/Comments";
 
+//https://www.youtube.com/watch?v=YiIfiVWmgmk&ab_channel=OktenSchool
 function App() {
     return (
-        <div>
-            ййййййййй
+        <Router>
+            <div>
+                <div><Link to="/">home page</Link></div>
+                <div><Link to="/users">users page</Link></div>
+                <div><Link to="/posts">post page</Link></div>
+                <div><Link to="/comments">comments page</Link></div>
 
-        </div>
+
+                {/*               exact - use for shortest links*/}
+                <Route path={'/'} exact render={() => {
+                    return <div>home</div>
+                }}/>
+
+                <Route path={'/users'} render={(props) => {
+                    console.log(props);
+                    return <Users {...props}/>;
+                }}/>
+
+                <Route path={'/posts'} component={Posts}/>
+         {/*     Switch - use for the same path, its filtering only first path*/}
+                <Switch>
+                    <Route path={'/comments'}>
+                        <Comments/>
+                    </Route>
+                    <Route path={'/comments'}>
+                        <Comments/>
+                    </Route>
+                </Switch>
+
+
+            </div>
+        </Router>
     );
 }
 
