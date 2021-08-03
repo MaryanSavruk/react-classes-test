b8d7abf84fcec07a69e93f63acc0d8b8b2848a28
savruk.maryan@gmail.com <savruk.maryan@gmail.com>
2021-08-03 23:44:19 +0300

    redux-1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
 import React from "react";
+import {useDispatch, useSelector} from "react-redux";
+
+const NestedChild = () => {
+    const counter = useSelector(({counter: {value}}) => value)
+    const dispatch = useDispatch();
+
+    return (
+        <header>
+        <h1>{counter}</h1>
+        <button onClick={() => {
+            dispatch({type: 'INC'})
+        }}>inc</button>
+
+        </header>
+
+    )
+}
 
 function App() {
-    return (
-        <div>
-            ййййййййй
 
+
+    return (
+
+        <div>
+
+            <NestedChild/>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,49 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+const initialValue = {
+    counter: {
+        value: 0
+    }
+}
+
+const reducer = (state = initialValue, action) => {
+    switch (action.type) {
+        case 'INC': {
+            return {
+                ...state,
+                counter: {
+                    value: state.counter.value + 1
+                }
+            }
+        }
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+const INCREMENT_ACTION = {
+    type: 'INC'
+}
+store.subscribe(() => {
+    console.log(store.getState())
+})
+
+// store.dispatch(INCREMENT_ACTION);
+// store.dispatch(INCREMENT_ACTION);
+// store.dispatch(INCREMENT_ACTION);
+console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
